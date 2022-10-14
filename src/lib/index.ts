45010bc197ab7ccc5c058eb703a22c654dcd3337
Jojoshua <jojoshua@gmail.com>
2022-10-14 14:52:29 -0400

    update exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,13 @@ import { DefinitionManager } from '$lib/lib/DefinitionManager';
 import { convertDataAttributes } from '$lib/Utils/Utils';
 import ComponentLabel from '$lib/Utils/ComponentUtilities/ComponentLabel.svelte';
 import GroupSlot from '$lib/Utils/ComponentUtilities/GroupSlot.svelte';
+import type { ThemeMap } from '$lib/Utils/Misc/Theme';
 
-export { FormBuilder, GroupSlot, ComponentLabel, DefinitionManager, convertDataAttributes };
+export {
+	FormBuilder,
+	GroupSlot,
+	ComponentLabel,
+	DefinitionManager,
+	convertDataAttributes,
+	ThemeMap
+};
