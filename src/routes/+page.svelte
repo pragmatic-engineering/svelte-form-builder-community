6112f234b84a5de10b13c68b617e8f7eb15c09bd
Jojoshua <jojoshua@gmail.com>
2022-10-21 09:36:04 -0400

    remove unnecessary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,6 @@
 		FormTab
 	} from '$lib/Utils/types';
 	import { capitalizeFirstLetter } from '$lib/Utils/Utils';
-	import type { Editor } from 'tinymce';
 
 	let formJSON2: FormDefinition[] = [
 		{
@@ -504,100 +503,6 @@
 						};
 					}
 				}
-			},
-			{
-				componentName: 'Signature',
-				customAttribute: {
-					penColor: 'red'
-				}
-			},
-			{
-				componentName: 'RichText',
-				customAttribute: {
-					// scriptSrc: "https://cdn.jsdelivr.net/npm/tinymce@6.2.0/tinymce.min.js",
-					conf: {
-						branding: false
-					}
-				},
-				customEvents: {
-					init: (e: Event, editor: Editor) => {
-						document.querySelector('.tox-promotion')?.remove();
-					}
-				}
-			},
-			{
-				importPath: '../Tests/Components/ExternalTest.svelte',
-				componentName: 'External',
-				icon: '✒️',
-				validateDefinition: (fieldInfo: FieldInfo): ValidationResult => {
-					if (!fieldInfo.field.htmlAttributes.class?.includes('test')) {
-						return {
-							field: fieldInfo.field,
-							errors: [
-								`(Tab ${fieldInfo.tab?.label}) ${fieldInfo.field.labelAttributes?.label} must contain a 'test' Class`
-							]
-						};
-					}
-				}
-			},
-			{
-				importPath: '../Tests/Components/ExternalCheckbox.svelte',
-				componentName: 'Pretty Checkbox',
-				icon: '<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="green" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M384 32C419.3 32 448 60.65 448 96V416C448 451.3 419.3 480 384 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H384zM339.8 211.8C350.7 200.9 350.7 183.1 339.8 172.2C328.9 161.3 311.1 161.3 300.2 172.2L192 280.4L147.8 236.2C136.9 225.3 119.1 225.3 108.2 236.2C97.27 247.1 97.27 264.9 108.2 275.8L172.2 339.8C183.1 350.7 200.9 350.7 211.8 339.8L339.8 211.8z"/></svg>'
-			},
-			{
-				importPath: '../Tests/Components/Svelte-Select.svelte',
-				componentName: 'Svelte Select',
-				icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-check" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
-        </svg>`,
-				disableStandardHTMLValidityCheck: true,
-				disabledQuickMenu: { copy: true, delete: true },
-				htmlAttributes: { multiple: false },
-				dataAttributes: [
-					{
-						label: 'Custom Options',
-						name: 'custom7',
-						options: [
-							{ label: 'Red', value: 'option-red' },
-							{ label: 'Green', value: 'option-green' },
-							{ label: 'Blue', value: 'option-blue' }
-						]
-					}
-				]
-			},
-			{
-				importPath: '../Tests/Components/AddressGroup.svelte',
-				categories: [],
-				componentName: 'Address',
-				icon: '✉️',
-				fieldGroup: [
-					{
-						fields: [
-							{
-								componentName: 'Text',
-								labelAttributes: { label: 'Name' },
-								htmlAttributes: {}
-							}
-						]
-					},
-					{
-						fields: [
-							{ componentName: 'Text', htmlAttributes: {}, labelAttributes: { label: 'Address' } }
-						]
-					},
-					{
-						fields: [
-							{ componentName: 'Text', htmlAttributes: {}, labelAttributes: { label: 'City' } },
-							{ componentName: 'Text', htmlAttributes: {}, labelAttributes: { label: 'State' } },
-							{
-								componentName: 'Number',
-								htmlAttributes: {},
-								labelAttributes: { label: 'Zip Code' }
-							}
-						]
-					}
-				]
 			}
 		],
 		formDefinition: formJSON2,
